76aa19ccbaf53cd01b77f7f9118f31121d040ef6
kimura-tsuyoshi <maxim_4141.rock@zoho.com>
2018-05-08 00:17:11 +0900

    [modify] TwitterImgSearch.php作成に伴って、実行スクリプトを変更。

--- Execute.php
+++ Execute.php
@@ -1,7 +1,9 @@
 <?php
 declare(strict_types=1);
+
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 require_once dirname(__FILE__) . '/class/Twitter.php';
+require_once dirname(__FILE__) . '/class/Slack.php';
 
 if (empty($argv[1]) || !is_string($argv[1])) {
     echo 'STOP ' . PHP_EOL;
@@ -12,7 +14,11 @@ if (empty($argv[1]) || !is_string($argv[1])) {
 $dotEnv = new Dotenv\Dotenv(__DIR__);
 $dotEnv->load();
 
-$command = $argv[1];
+$command  = $argv[1];
+$fileName = $command . '.php';
+if (!file_exists($fileName)) {
+    throw new RuntimeException('Not Found File : ' . $fileName);
+}
 
 require_once dirname(__FILE__) . '/' . $command . '.php';
 
@@ -20,12 +26,21 @@ if (!class_exists($command)) {
     throw new RuntimeException('Not Found Class : ' . $command);
 }
 
-// バッチ処理の実行
-$timeZone = new \DateTimeZone('Asia/Tokyo');
-echo 'START : ' . (new \DateTime('now', $timeZone))->format('Y-m-d H:i:s') . PHP_EOL;
+try {
+    // バッチ処理の実行
+    $timeZone = new \DateTimeZone('Asia/Tokyo');
+    echo 'START : ' . (new \DateTime('now', $timeZone))->format('Y-m-d H:i:s') . PHP_EOL;
 
-$obj   = new $command();
-$result = $obj->execute();
+    $obj    = new $command();
+    $result = $obj->execute();
 
-echo $result . PHP_EOL;
-echo 'END : ' . (new \DateTime('now', $timeZone))->format('Y-m-d H:i:s') . PHP_EOL;
+    echo $result . PHP_EOL;
+    echo 'END : ' . (new \DateTime('now', $timeZone))->format('Y-m-d H:i:s') . PHP_EOL;
+} catch (\Abraham\TwitterOAuth\TwitterOAuthException $e) {
+    $message = 'Fail : ' . $command . PHP_EOL;
+    $message .= (new \DateTime('now', $timeZone))->format('Y-m-d H:i:s') . PHP_EOL;
+    $message .= $e->getMessage() . PHP_EOL;
+
+    $slack = new Slack();
+    $slack->sendToSlack($message);
+}

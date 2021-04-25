a4b8e84cfd9175bfda9261ac656b7c180f38aed3
juve534 <maxim_4141.rock@zoho.com>
2021-04-26 01:00:35 +0900

    :wrench: brefの操作を簡略化
    シェルとMakefileで操作を簡略化.

---
+++ Makefile
@@ -0,0 +1,21 @@
+container=app
+
+init:
+	docker-compose up -d
+	docker-compose exec $(container) composer install
+	sh init.sh
+
+up:
+	docker-compose up -d
+
+down:
+	docker-compose down
+
+composer:
+	docker-compose exec $(container) composer $(CMD)
+
+bref:
+	docker-compose exec $(container) vendor/bin/bref $(CMD)
+
+sls:
+	docker-compose exec $(container) serverless $(CMD)
---
+++ init.sh
@@ -0,0 +1,25 @@
+#/bin/sh
+
+set -a
+source .env
+set +a
+
+echo "アクセスキーを指定してください"
+echo -n 'aws_access_key_id: '
+read aws_access_key_id
+
+echo "シークレットアクセスキーを指定してください"
+echo -n 'aws_secret_access_key: '
+read aws_secret_access_key
+
+if [[ -n "${aws_access_key_id}" ]];then
+  echo 'アクセスキーが指定されてません'
+  exit 1
+fi
+
+if [[ -n "${aws_secret_access_key}" ]];then
+  echo 'シークレットアクセスキーが指定されてません'
+  exit 1
+fi
+
+docker-compose exec app serverless config credentials --provider aws --key $aws_access_key_id --secret $aws_secret_access_key
